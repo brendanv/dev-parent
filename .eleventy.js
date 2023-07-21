3231dcd333f148e7b466a48367bf66bb57c9c658
Brendan Viscomi <github@bvlmnop.com>
2023-07-20 22:24:34 -0400

    Move to content folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./public/": "/"
   });
+
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      data: "../_data"
+    }
+  };
 };
 
