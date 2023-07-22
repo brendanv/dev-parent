71b78b846aa36bce73f6f3103232384a62aa7057
Brendan Viscomi <github@bvlmnop.com>
2023-07-21 22:22:55 -0400

    Add post list to home page

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,23 @@ module.exports = function(eleventyConfig) {
 		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
 	});
 
+	// Get the first `n` elements of a collection.
+	eleventyConfig.addFilter("head", (array, n) => {
+		if(!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if( n < 0 ) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+
+	// Return the smallest number argument
+	eleventyConfig.addFilter("min", (...numbers) => {
+		return Math.min.apply(null, numbers);
+	});
+
   return {
     dir: {
       input: "content",
--- content/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: layouts/base.njk
----
-
-<a href="blog/firstpost">Test link</a>
-
