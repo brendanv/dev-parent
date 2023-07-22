4c30779b96cac4d60b866e6fa5b217fad1b5a06c
Brendan Viscomi <github@bvlmnop.com>
2023-07-21 22:18:28 -0400

    basic post list page at /blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+const { DateTime } = require("luxon")
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginBundle);
+
+  eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+	});
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+	});
 
   return {
     dir: {
